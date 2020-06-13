e5c771368000031caaf63c5826878c8a28da544a
Mona Alrashidi <63103306+MonaAlrashidi@users.noreply.github.com>
2020-06-13 19:17:54 +0300

    cw

---
+++ Classwork-1-MonaAlrashidi-609.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+let name = "Mona"
+
+var hobby1 = "Singing"
+
+var hobby2 = "Reading"
+
+var age = 16
+
+var height = 1.60
+
+print("Hello my name is" , name , "I enjoy" , hobby1 , "and", hobby2 , ". I  am" , age , "years old and my height is" , height ,".")
+
+//let firendName = "Lujain"
+
+//var friendHobby1 = "Programming"
+
+//var friendHobby2 = "Swimming"
+
+//var friendAge = 17
+
+//var friendHeight = 1.60
+
+//print("Hello my  friends name is" , firendName , "she enjoys" , friendHobby1 , "and", friendHobby2 , ". She is" , friendAge , "years old and her height is" , friendHeight ,".")
+
+
+
